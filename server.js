3470c433c7d63899bf8e993f6430f62df56d2a9b
Armaan Kara <vancouverarmaan@gmail.com>
2021-11-20 15:38:35 -0800

    make it better heroku2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 const app = express();
 const path = require("path")
 
